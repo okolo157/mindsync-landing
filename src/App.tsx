59f90133c9e73dee3e1b7067bafc9e08b63d238c
okolo157 <victor.okolo@pau.edu.ng>
2026-02-28 10:25:39 +0100

    feat: migrate and wire up secondary navbar routes (Products, Plans, Contact, etc)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "@/pages/Landing";
+import Products from "@/pages/Products";
+import Plans from "@/pages/Plans";
+import Contact from "@/pages/Contact";
+import ScheduleDemo from "@/pages/ScheduleDemo";
+import Privacy from "@/pages/Privacy";
+import Terms from "@/pages/Terms";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 function App() {
@@ -8,6 +14,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/plans" element={<Plans />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/schedule-demo" element={<ScheduleDemo />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
